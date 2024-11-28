1101a3b3d47a47ea1adc2eee1033a41fb45576a9
TRIDIB DAWN <tridib.dawn@nextbeemedia.com>
2024-11-28 21:36:18 +0530

    back button added

--- src/views/CompanyDetails.vue
+++ src/views/CompanyDetails.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="container mx-auto py-8">
+      <router-link to="/" class="text-blue-500 hover:underline mb-4 inline-block">
+        <button class="bg-gray-200 text-gray-800 py-2 px-4 rounded hover:bg-gray-300 transition duration-300">
+          &larr; Back
+        </button>
+      </router-link>
       <h1 class="text-4xl font-bold text-center mb-6">Company Details</h1>
       <div v-if="loading" class="flex justify-center items-center min-h-screen">
         <div class="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32"></div>
